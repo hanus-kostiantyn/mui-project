7e63f4fff67ee3be20114bffa4463b23e83a649a
hanus-kostiantyn <hanus.kostiantyn@gmail.com>
2024-10-03 22:33:31 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Button, Typography, styled } from "@mui/material";
+import SettingsRoundedIcon from "@mui/icons-material/SettingsRounded";
+import AddRoundedIcon from "@mui/icons-material/AddRounded";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const BlueButton = styled(Button)(({ theme }) => ({
+        backgroundColor: theme.palette.otherColor.main,
+        color: "#888",
+        margin: 5,
+        "&:hover": {
+            backgroundColor: "lightblue",
+        },
+        "&:disabled": {
+            backgroundColor: "gray",
+            color: "white",
+        },
+    }));
+    return (
+        <div className="App">
+            <Button variant="text">Text</Button>
+            <Button
+                startIcon={<AddRoundedIcon />}
+                variant="contained"
+                color="otherColor"
+            >
+                Settings
+            </Button>
+            <Button variant="outlined">Outlined</Button>
+            <Typography variant="h1">h1. Heading</Typography>
+
+            <BlueButton>My Button</BlueButton>
+            <BlueButton>Another Button</BlueButton>
+        </div>
+    );
 }
 
 export default App;
